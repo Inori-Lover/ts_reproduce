128a3100e0eb2f707de8ad8bb99b86ac97751096
Inori <337938188@qq.com>
2018-08-09 19:47:00 +0800

    修改组件注释

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const routeList = [
 ]
 
 /**
- * @func default 路由加载基础组件，各自模块在各自二级路由
+ * @func default 程序整体初始化入口，目前技术栈而言就是路由入口了
  * @url https://segmentfault.com/a/1190000011765141#articleHeader17
  */
 export default () => (
