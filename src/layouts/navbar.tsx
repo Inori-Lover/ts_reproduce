8b97bafac11a02c033079183b0dffc838380e878
Inori <337938188@qq.com>
2018-08-23 15:02:21 +0800

    实现键盘弹起时隐藏的功能，纠结是否对外暴露控制显隐的接口

--- src/layouts/navbar.tsx
+++ src/layouts/navbar.tsx
@@ -1,4 +1,4 @@
-import React, { SFC, ReactNode } from 'react'
+import React, { ReactNode } from 'react'
 import { Link } from 'react-router-dom'
 
 import { withDefaultProps } from "../tools/defaultProps";
@@ -24,35 +24,77 @@ export type Props = {
   children: ReactNode,
   navs?: navbarItem[] | void[],
   hideonKeyboardPopup?: boolean,
-  active: string
+  active?: string
 } & DefaultProps
 
-const _Page: SFC<Props> = (props) => {
-  const { children, navs, active } = props
-  return (
-    <div className="layout--navbar_container">
-      <div className="layout--navbar_content">{ children }</div>
-      <div className="layout--navbar placeholder">
-      </div>
-      <div className="layout--navbar">
-        {
-          navs.map((item: any, index: number) => {
-            const { icon, title, url } = item
-            return (
-              <Link key={index} className={`layout--navbar_item_container ${active === title ? 'active' : ''}`} to={url ? url : ''} replace >
-                <div className={`layout--navbar_item_icon`}>
-                  {
-                    icon && <img src={icon} alt=""/>
-                  }
-                </div>
-                <div className={`layout--navbar_item`}>{title}</div>
-              </Link>
-            )
-          })
+type State = {
+  initalHeight: number
+  hide: boolean
+}
+
+const initalState: State = {
+  initalHeight: window.innerHeight,
+  hide: false,
+}
+
+class _Page extends React.PureComponent<Props, State> {
+
+  readonly state: State = initalState
+
+  public componentDidMount () {
+    window.onresize = this.resizeHandle
+    this.setState(function () {
+      return {
+        initalHeight: window.innerHeight
+      }
+    })
+  }
+
+  private resizeHandle = () => {
+    const shouldHide = window.innerHeight < this.state.initalHeight * 0.9
+    if (shouldHide) {
+      this.setState(function () {
+        return {
+          hide: true
         }
+      })
+    } else {
+      this.setState(function () {
+        return {
+          hide: false
+        }
+      })
+    }
+  }
+
+  public render () {
+    const { children, navs, active, hideonKeyboardPopup } = this.props
+
+    return (
+      <div className="layout--navbar_container">
+        <div className="layout--navbar_content">{ children }</div>
+        <div className={`layout--navbar placeholder ${this.state.hide && hideonKeyboardPopup ? 'hidden' : ''}`}>
+        </div>
+        <div className={`layout--navbar ${this.state.hide && hideonKeyboardPopup ? 'hidden' : ''}`}>
+          {
+            navs.map((item: any, index: number) => {
+              const { icon, title, url } = item
+              return (
+                <Link key={index} className={`layout--navbar_item_container ${active === title ? 'active' : ''}`} to={url ? url : ''} replace >
+                  <div className={`layout--navbar_item_icon`}>
+                    {
+                      icon && <img src={icon} alt=""/>
+                    }
+                  </div>
+                  <div className={`layout--navbar_item`}>{title}</div>
+                </Link>
+              )
+            })
+          }
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 export const Page = withDefaultProps(defaultProps, _Page)
 export default Page
