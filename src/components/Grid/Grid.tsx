b9aaef17111bcdf6f2bbe54e3f968218d59d4393
Inori <337938188@qq.com>
2018-08-31 11:47:18 +0800

    添加网格组件

---
+++ src/components/Grid/Grid.tsx
@@ -0,0 +1,38 @@
+/**
+ * 网格组件
+ */
+
+// 库
+import React, { SFC } from 'react'
+
+// 工具
+
+// 资源
+import '../base.scss'
+import './Grid.scss'
+
+/**
+ * 全量Props类型定义
+ */
+type Props = {
+  peerLine: number
+  children: JSX.Element[]
+}
+
+
+/**
+ * @class SearchBar
+ * @desc 搜索栏
+ */
+export const Grid: SFC<Props> = function ({ peerLine, children }) {
+  return (
+    <div className={`inori-grid_container`} style={{color: `#${peerLine}`}}>{
+      children.map(item => {
+        return (
+          <div className="inori-grid_item" style={{flexBasis: `${100 / peerLine}%`}}>{item}</div>
+      )})
+    }</div>
+  )
+}
+
+export default Grid
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 
 import SearchBar from '../components/SearchBar/SearchBar'
 import Swiper, { swiperItemProps as SwiperProps } from '../components/Swiper'
+import { Grid } from "../components/Grid/Grid";
 
 const initialState = { time: Date.now() }
 
@@ -36,6 +37,22 @@ export default class Page extends React.PureComponent {
         <form action="https://www.baidu.com/s" method="get">
           <SearchBar name="wd" />
         </form>
+        <Grid peerLine={ 5 }>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <Grid peerLine={ 2 }>
+            <div>test1</div>
+            <div>test1</div>
+            <div>test1</div>
+          </Grid>
+        </Grid>
       </>
     )
   }
